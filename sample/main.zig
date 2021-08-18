490d2828bba06a2007785fb85bad23af344e9471
Lee Cannon <leecannon@leecannon.xyz>
2021-08-18 15:27:20 +0100

    store nullable repo pointer

--- sample/main.zig
+++ sample/main.zig
@@ -5,7 +5,7 @@ pub fn main() !void {
     const handle = try git.init();
     defer handle.deinit();
 
-    const repo = try handle.openRepository("/home/lee/empty_repo");
+    var repo = try handle.openRepository("/home/lee/empty_repo");
     defer repo.deinit();
 }
 
